9469a439698b7b596fdeef4b49137834c6c3a560
kayandrewj <kayandrewj@gmail.com>
2017-09-24 17:08:27 -0400

    add cl card rendering

--- cards.py
+++ cards.py
@@ -1,3 +1,18 @@
+DISPLAY_VALUES = {
+1: "A",
+13: "K",
+12: "Q",
+11: "J",
+2: "2",
+3: "3",
+4: "4",
+5: "5",
+6: "6",
+7: "7",
+8: "8",
+9: "9",
+10: "10"
+}
 def create_deck():
     values = list(range(1, 14));
     suits = ["S", "H", "D", "C"]
@@ -7,4 +22,17 @@ def create_deck():
         for value in values:
             deck.append((suit, value))
 
+    row = "|      |"
+    top = "________"
+    bottom = "--------"
+    for card in deck:
+        disp_val = DISPLAY_VALUES[card[1]]
+        data_row = "|{0}    {1}|".format(card[0], disp_val)
+        print(top)
+        print(data_row)
+        print(row)
+        print(row)
+        print(data_row)
+        print(bottom)
+        print(" ")
 create_deck()
